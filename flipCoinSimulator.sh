05778f90a7fc3e4ad881d0ae30607a521d13da51
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 13:20:34 +0530

    Multiple times of Flipping a coin through loop and showing result

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-#coin toss
-coinToss=$((RANDOM%2))
-if [ $coinToss == 0 ]
-then
-   echo "Heads"
-else
-   echo "Tails"
-fi
+
+read -p "Enter how many times you wnat to flip " count
+heads=0
+tails=o
+
+for ((i=0; i<$count; i++ ))
+do
+   flip=$((RANDOM%2))
+
+   if [ $flip -eq 1 ]
+   then
+      (( heads++ ))
+   else
+      (( tails++ ))
+   fi
+
+done
+#echo "COUNT-"$count
+echo "HEAD_WINS-"$heads
+echo "TAIL_WINS-"$tails
